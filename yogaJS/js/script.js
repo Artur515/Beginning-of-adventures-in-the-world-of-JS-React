598f6d1f14cae564e315c6355cf7a70a4da72ffc
Artur515 <drugartur5@gmail.com>
2020-10-14 18:42:38 +0300

    My first timer мой первый таймер,можно использовать

--- yogaJS/js/script.js
+++ yogaJS/js/script.js
@@ -35,4 +35,52 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
+
+  //TIMER JS create the timer to deadline
+  let deadline = "2020-10-15";
+
+  function getTimeRemaining(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date()); //parse превращает любую дату в кол-во миллисекунд
+    //мы взяли дату deadline (она когда-то будет) и отняли дату  когда пользователь зашел на сайт
+    //и в t лежит кол-во миллисекунд
+    let seconds = Math.floor((t / 1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
+    let hours = Math.floor(t / (1000 * 60 * 60));
+    //let hoursDay=Math.floor((t / 1000 / 60/60) % 24);
+    //let days =Math.floor((t / (1000 * 60 * 60*24)));
+
+    return {
+      total: t,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id);
+    let hours = timer.querySelector(".hours");
+    let minutes = timer.querySelector(".minutes");
+    let seconds = timer.querySelector(".seconds");
+    let timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+      function addZeroNum(num) {
+        if (num <= 9) {
+          return "0" + num;
+        } else return num;
+      }
+      hours.textContent = addZeroNum(t.hours);
+      minutes.textContent = addZeroNum(t.minutes);
+      seconds.textContent = addZeroNum(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+      }
+    }
+  }
+  setClock("timer", deadline);
 });
