ffde2637b95e557a42e5fdcc5965889ba2d06acd
Artur515 <drugartur5@gmail.com>
2021-02-05 20:18:58 +0300

    change form input

--- Hipstagramm_React_Project/hipstagramm_react_project/src/components/Pages/Main/SettingPage/Setting.jsx
+++ Hipstagramm_React_Project/hipstagramm_react_project/src/components/Pages/Main/SettingPage/Setting.jsx
@@ -4,44 +4,49 @@ import style from './style.module.css'
 const Setting = () => {
     return (
         <div>
-            <Grid columns={2} stackable className={style.div}>
+            <Grid columns={2} stackable>
                 <Grid.Row stretched>
                     <Grid.Column>
                         <Segment className={style.first_segment}><Image
                             src='https://react.semantic-ui.com/images/wireframe/square-image.png' size='small'
                             circular/><br/>
-                            <Button className={style.button}>Change photo</Button><br/>
-                            <label>Login</label>
-                            <Input size='large' placeholder='Login'/>
+                            <Button>Change photo</Button><br/>
                         </Segment>
                     </Grid.Column>
                     <Grid.Column>
-                        <Segment className={style.first_segment}>
-                            <label htmlFor="">First name</label>
-                            <Input size='large' labelPosition='left' placeholder='Enter first name'
-                                   className={style.input}/>
-                            <label htmlFor="">Last name</label>
-                            <Input size='large' placeholder='Enter last name' className={style.input}/>
-                            <label htmlFor="">Email</label>
-                            <Input size='large' placeholder='Enter email' className={style.input}/>
+                        <Segment>
+                            <form className={style.form}>
+                                <label>Login</label>
+                                <Input type='text' placeholder='Login' className={style.input}/>
+                                <label>First name</label>
+                                <Input type='text' placeholder='First name' className={style.input}/>
+                                <label>Last name</label>
+                                <Input type='text' placeholder='Last name' className={style.input}/>
+                                <label>Email</label>
+                                <Input type='email' placeholder='Email' className={style.input}/>
+                                <Button>Save profile</Button>
+                            </form>
                         </Segment>
 
                     </Grid.Column>
                 </Grid.Row>
-
                 <Grid.Column width={16}>
-                    <Segment><Button size='large'  color='black'>Save profile</Button> </Segment>
-                    <Segment className={style.third_segment}>
-                        <div className={style.input_password}>
-                            <label htmlFor="">Password</label>
-                            <Input size='large' placeholder='Search...'/>
-                        </div>
-                        <div className={style.input_password}>
-                            <label htmlFor="">Confirm password</label>
-                            <Input size='large' placeholder='Search...'/>
-                        </div>
+                    <Segment>
+                        <form className={style.form_password}>
+                            <div className={style.form_div}>
+                                <div className={style.div_input}>
+                                    <label>Password</label>
+                                    <Input type='password' placeholder='Password'/>
+                                </div>
+                                <div className={style.div_input}>
+                                    <label>Confirm Password</label>
+                                    <Input type='password' placeholder='Password'/>
+                                </div>
+                            </div>
+                            <Button>Save password</Button>
+                        </form>
                     </Segment>
-                    <Segment><Button  color='black' size='large'>Save password</Button></Segment>
+
                 </Grid.Column>
             </Grid>
         </div>
