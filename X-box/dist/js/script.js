900107b719a47b4045a82cd070c10a7531245e77
Artur <drugartur5@gmail.com>
2021-03-13 18:16:55 +0300

    add x-box lending

---
+++ X-box/dist/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.games_slider').slick({
+        infinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/icon-prev.png" alt="prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/icon-next.png" alt="next"></button>',
+
+
+        
+    });
+});
